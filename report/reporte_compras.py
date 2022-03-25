0926879bc5e564d97afb1be44b478d6c8be054f8
Rodolfo Borstcheff <rborst@gmail.com>
2022-03-25 16:40:29 +0000

    Commit inicial v.15

--- report/reporte_compras.py
+++ report/reporte_compras.py
@@ -21,7 +21,7 @@ class ReporteCompras(models.AbstractModel):
         journal_ids = [x for x in datos['diarios_id']]
         facturas = self.env['account.move'].search([
             ('state','in',['posted']),
-            ('type','in',['in_invoice','in_refund']),
+            ('move_type','in',['in_invoice','in_refund']),
             ('journal_id','in',journal_ids),
             ('date','<=',datos['fecha_hasta']),
             ('date','>=',datos['fecha_desde']),
